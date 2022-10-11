8412b631984726db88096edd834a7ab644c8d2f9
Mike Reinhard <mike@mikereinhard.com>
2022-10-10 20:22:55 -0700

    prelimnary attempt to add transcription to deploy script

--- make-deploy.sh
+++ make-deploy.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+download_audio () {
+    cat ../content/episodes.json | jq '.[0].file.url' | xargs -r -I{} curl -L {} -O
+}
+
 cd feed-parser/
 
-npm run parse && cd .. && npm run lint && npm run deploy
+npm run parse && \
+# download_audio && \
+cd .. && \
+npm run lint && \
+npm run deploy
