283c936d66016bf4627787d36f69a87fed956b7f
Mike Reinhard <mike@mikereinhard.com>
2022-10-23 18:20:54 -0700

    add force deploy option

--- make-deploy.sh
+++ make-deploy.sh
@@ -4,10 +4,15 @@ download_audio () {
     cat ../content/episodes.json | jq '.[0].file.url' | xargs -r -I{} curl -L {} -O
 }
 
-cd feed-parser/
+if [[ -z $1 || $1 -ne '-f' ]]; then
+    cd feed-parser/
 
-npm run parse && \
-# download_audio && \
-cd .. && \
-npm run lint && \
-npm run deploy
+    npm run parse && \
+    download_audio && \
+    cd ..  && \
+    npm run lint && \
+    npm run deploy
+else
+    npm run lint && \
+    npm run deploy
+fi
